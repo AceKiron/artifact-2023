07864e91be8df0ddb3fe01d84b23a1a2ce73bc8d
Ace <acekiron@outlook.com>
2023-01-31 15:00:29 +0100

    Knipperen met LEDs 2

--- src/main.py
+++ src/main.py
@@ -5,8 +5,12 @@ led_onboard = machine.Pin(25, machine.Pin.OUT)
 led_extern = machine.Pin(1, machine.Pin.OUT)
 
 while True:
-    led_onboard.toggle()
+    led_onboard.value(1)
+    time.sleep(.2)
+    led_extern.value(1)
     time.sleep(.2)
     
-    led_extern.toggle()
+    led_onboard.value(0)
+    time.sleep(.2)
+    led_extern.value(0)
     time.sleep(.2)
