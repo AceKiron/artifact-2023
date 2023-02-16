ddd1b03bbd821b1f5ddbf8395a3f4a4449d84311
Ace <acekiron@outlook.com>
2023-02-16 12:07:56 +0100

    Update picosquared.py

--- src/picosquared.py
+++ src/picosquared.py
@@ -4,36 +4,6 @@ from machine import Pin, I2C
 from micropython import const
 import framebuf
 
-class Enabler:
-    def __init__(self, lastEnabled=False, enabled=True):
-        self.lastEnabled = lastEnabled
-        self.enabled = enabled
-
-        self.Update()
-    
-    def OnEnable(self):
-        pass
-    
-    def OnDisable(self):
-        pass
-    
-    def Enable(self):
-        self.enabled = True
-    
-    def Disable(self):
-        self.enabled = False
-
-    def IsEnabled(self):
-        return self.enabled
-    
-    def Update(self):
-        if self.enabled and not self.lastEnabled:
-            self.OnEnable()
-        elif self.lastEnabled and not self.enabled:
-            self.OnDisable()
-        
-        self.lastEnabled = self.enabled
-
 class Application:
     def __init__(self):
         self.SleepTime = 0
@@ -68,36 +38,6 @@ class Input:
     
     def Print(self):
         print(self.pin.value())
-    
-class Output:
-    def __init__(self, pin):
-        if pin != None:
-            self.pin = Pin(pin, Pin.OUT)
-            
-            self.WriteLow()
-    
-    def Read(self):
-        return self.pin.value()
-    
-    def Print(self):
-        print(self.pin.value())
-    
-    def Toggle(self):
-        self.pin.toggle()
-
-    def WriteHigh(self):
-        self.pin.value(1)
-    
-    def WriteLow(self):
-        self.pin.value(0)
-
-class Button(Input):
-    def __init__(self, pin):
-        super().__init__(pin)
-
-class LED(Output):
-    def __init__(self, pin):
-        super().__init__(pin)
 
 class Keypad4x4(Input):
     Keys = ["1", "2", "3", "A",
@@ -129,8 +69,6 @@ class Keypad4x4(Input):
                 
             self.row_pins[row].low()
 
-# Subclassing FrameBuffer provides support for graphics primitives
-# http://docs.micropython.org/en/latest/pyboard/library/framebuf.html
 class SSD1306(framebuf.FrameBuffer):
     # register definitions
     SET_CONTRAST = const(0x81)
@@ -150,7 +88,7 @@ class SSD1306(framebuf.FrameBuffer):
     SET_PRECHARGE = const(0xD9)
     SET_VCOM_DESEL = const(0xDB)
     SET_CHARGE_PUMP = const(0x8D)
-    
+
     def __init__(self, width, height, external_vcc):
         self.width = width
         self.height = height
@@ -159,7 +97,7 @@ class SSD1306(framebuf.FrameBuffer):
         self.buffer = bytearray(self.pages * self.width)
         super().__init__(self.buffer, self.width, self.height, framebuf.MONO_VLSB)
         self.init_display()
- 
+
     def init_display(self):
         for cmd in (
             self.SET_DISP | 0x00,  # off
@@ -196,20 +134,20 @@ class SSD1306(framebuf.FrameBuffer):
             self.write_cmd(cmd)
         self.fill(0)
         self.show()
- 
+
     def poweroff(self):
         self.write_cmd(self.SET_DISP | 0x00)
- 
+
     def poweron(self):
         self.write_cmd(self.SET_DISP | 0x01)
- 
+
     def contrast(self, contrast):
         self.write_cmd(self.SET_CONTRAST)
         self.write_cmd(contrast)
- 
+
     def invert(self, invert):
         self.write_cmd(self.SET_NORM_INV | (invert & 1))
- 
+
     def show(self):
         x0 = 0
         x1 = self.width - 1
@@ -224,8 +162,7 @@ class SSD1306(framebuf.FrameBuffer):
         self.write_cmd(0)
         self.write_cmd(self.pages - 1)
         self.write_data(self.buffer)
- 
- 
+
 class SSD1306_I2C(SSD1306):
     def __init__(self, width, height, scl, sda, freq=200000, addr=0x3C, external_vcc=False):
         self.i2c = I2C(0, scl=Pin(scl), sda=Pin(sda), freq=freq)
@@ -233,12 +170,12 @@ class SSD1306_I2C(SSD1306):
         self.temp = bytearray(2)
         self.write_list = [b"\x40", None]  # Co=0, D/C#=1
         super().__init__(width, height, external_vcc)
- 
+
     def write_cmd(self, cmd):
         self.temp[0] = 0x80  # Co=1, D/C#=0
         self.temp[1] = cmd
         self.i2c.writeto(self.addr, self.temp)
- 
+
     def write_data(self, buf):
         self.write_list[1] = buf
         self.i2c.writevto(self.addr, self.write_list)
